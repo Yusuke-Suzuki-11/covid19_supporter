9be84342f6c7c6178c6749430b67f429f37b8345
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-10-02 21:05:39 +0900

    スタイル保存

---
+++ lib/config/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const TextStyle buttonTextStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600
+  );
+  
+  static const TextStyle chartLabelsTextStyle = TextStyle(
+    color: Colors.grey,
+    fontSize: 14,
+    fontWeight: FontWeight.w500
+  );
+  
+  static const TextStyle tabTextStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600
+  );
+}
